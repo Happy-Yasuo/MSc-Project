fd71e4aade36e8ef212c7b06a26e8c03f0a5e433
Happy-Yasuo <54522678+Happy-Yasuo@users.noreply.github.com>
2020-06-24 12:24:32 +0100

    Update DQNModule.py

--- convlab2/policy/DQNModule.py
+++ convlab2/policy/DQNModule.py
@@ -74,56 +74,24 @@ def read_action_map(file_path, da_dim=209):
     return position_dict, act_vec_dict
 
 
-def expert_act_vec2ind(cur_exp_act, position_dict, rand='True'):
+def expert_act_vec2ind(cur_exp_act, position_dict):
     """transform an expert action into an index in DQN action space"""
     """cur_exp_act is the indexes list of an expert action vector with 209 dimensions"""
     num_act = len(cur_exp_act)
     # -1 means we fail to map the expert action into an index in our action space
     cur_act_ind = -1
-    num_same_action = {}
     if num_act == 1:
         cur_act_ind = list(position_dict.keys())[list(position_dict.values()).index([cur_exp_act])]
     elif num_act > 1:
-        cur_exp_set = set(cur_exp_act)
         for idx in position_dict:
             cur_comb_len = len(position_dict[idx][0])
-            if cur_comb_len > num_act:
-                continue
-            elif cur_comb_len == num_act:
+            if cur_comb_len == num_act:
                 for act_vec in position_dict[idx]:
                     if act_vec == cur_exp_act:
                         cur_act_ind = idx
                         break
                 if cur_act_ind != -1:
                     break
-            else:
-                best_similarity = 0
-                for act_vec in position_dict[idx]:
-                    act_vec_set = set(act_vec)
-                    if act_vec_set <= cur_exp_set:
-                        similarity = len(act_vec_set & cur_exp_set)
-                        if similarity > best_similarity:
-                            best_similarity = similarity
-                    else:
-                        continue
-                num_same_action[idx] = best_similarity
-
-    if num_act > 0:
-        if cur_act_ind == -1:
-            similarity_lst = list(num_same_action.items())
-            similarity_lst.sort(key=lambda x: x[1])
-            similarity_lst.reverse()
-            candidate = []
-            max_score = similarity_lst[0][1]
-            for i in range(list(num_same_action.values()).count(max_score)):
-                candidate.append(similarity_lst[i][0])
-            if max_score > num_act-2:
-                if rand:
-                    cur_act_ind = random.choice(candidate)
-                else:
-                    cur_act_ind = candidate[0]
-            else:
-                cur_act_ind = -1
     else:
         cur_act_ind = -1
     return int(cur_act_ind)
@@ -149,13 +117,16 @@ class ExperienceReplay(object):
         """use this method to add real experience"""
         self.memory.append(Transition_new(*args))
 
-    def append(self, new_memory):
+    def append(self, new_memory, expert=False):
         """use this method to add new memory from interacting with environment and keep the total size under maximum"""
-        self.memory = new_memory.memory + self.memory
-        if self.__len__() > (self.max_size - len(self.expert_demo)):
-            num_del = self.__len__() - (self.max_size - len(self.expert_demo))
-            for _ in range(num_del):
-                self.memory.pop()
+        if expert:
+            self.expert_demo = new_memory.expert_demo + self.expert_demo
+        else:
+            self.memory = new_memory.memory + self.memory
+            if self.__len__() > (self.max_size - len(self.expert_demo)):
+                num_del = self.__len__() - (self.max_size - len(self.expert_demo))
+                for _ in range(num_del):
+                    self.memory.pop()
 
     def get_batch(self, batch_size=None):
         all_data = self.expert_demo + self.memory
